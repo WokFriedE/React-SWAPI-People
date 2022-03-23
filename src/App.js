b3cdfa26599d69d55fd85407dee1f32765960efe
WokFriedE <47959663+WokFriedE@users.noreply.github.com>
2022-03-23 16:14:50 -0400

    Testing Items

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+// import { Button, Alert } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   const [people, setPeople] = useState([])
 
   useEffect(() => {
-    async function fetchPeople() {
+    async function fetchPeople(param) {
       let res = await fetch('https://swapi.dev/api/people')
       let data = await res.json();
       setPeople(data.results);
+      console.log("confirm")
     }
 
-    fetchPeople();
+    fetchPeople("1")
+
   }, [])
+
+  const sortedPeople = Object.keys(people).sort();
   console.log('people', people);
   console.log("data", people);
+  console.log("sort data", sortedPeople);
 
-  return (
-    <div className="main">
-
-    </div>
-  );
+  {
+    return (
+      <div>
+        <p>
+          {people.name}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default App;
