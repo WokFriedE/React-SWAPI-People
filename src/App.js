b26a36ea71698865c896886818273e41b535b2c2
WokFriedE <ethankh717@gmail.com>
2022-03-27 16:14:40 -0400

    Pagination, Page Sorted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import { Button, Card, Pagination, PageItem } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import PageBar from './components/PaginationComp.js';
 import People from './components/People.js';
-// import { Button, Alert } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import PaginationComp from './components/PaginationComp.js';
 
 function App() {
-
+  // Information on every person
   let [allPeople, setAllPeople] = useState([]);
-  let [indexesNames, setIndexesNames] = useState([]);
-  let indexes = {};
+  let peopleInfo = {};
+
   let [pages, setPages] = useState([]);
+  let seperatePages = {};
   let nextPage = 'https://swapi.dev/api/people/?page=1';
 
-  useEffect(() => {
+  //Pagination
+  let active = 1;
+  let items = [];
 
+
+  useEffect(() => {
     console.log("fetching data");
+
     async function fetchAllData(param) {
 
       let res = await fetch(param);
@@ -41,8 +50,9 @@ function App() {
     fetchAllData(nextPage);
   }, [])
 
+  //generates a dictionary of names and their attributes needed
   for (let i = 0; i < allPeople.length; i++) {
-    indexes[allPeople[i].name] = {
+    peopleInfo[allPeople[i].name] = {
       "gender": allPeople[i].gender,
       "hair_color": allPeople[i].hair_color,
       "height": allPeople[i].height,
@@ -50,47 +60,57 @@ function App() {
     }
   }
 
-  console.log(indexes);
-  // indexes = Array.from(new Set(indexes));
-  var sortedNames = Object.keys(indexes).sort();
+  //creates the alphabeticalized list of names
+  var sortedNames = Object.keys(peopleInfo).sort();
 
-  // console.log(allPeople);
-
-  // console.log(indexes);
-  console.log(sortedNames);
-  // console.log(pages);
-
-
-  for (let i = 0; i < pages; i++) {
+  //creates the list of people for each page
+  for (let i = 1; i <= pages; i++) {
     sortedNames.map((person, x) => {
-
+      if (x < 10 * i && x >= 10 * (i - 1)) {
+        seperatePages[i] = {
+          ...seperatePages[i],
+          [person]: peopleInfo[person]
+        }
+      }
     })
   }
 
+  //sets the pages aviable for pagination
+  for (let i = 1; i <= pages; i++) {
+    items.push(
+      <Pagination.Item key={i} active={i === active}>
+        {i}
+      </Pagination.Item>
+    );
+  }
 
+  console.log(seperatePages);
 
   return (
     <>
+      {/* <PaginationComp pages={pages} /> */}
+      <Pagination>{items}</Pagination>
+      {/* <People pageCompleted={seperatePages} page={active} /> */}
 
-      {sortedNames.map((person, i) => { //make for loop that will loop through all people indexes, use page algorithm (index+10*page)
+      {/* {sortedNames.map((person, i) => { //make for loop that will loop through all people indexes, use page algorithm (index+10*page)
         return (
-          // loop through cards
+          loop through cards
           <div key={i}>
             <h2>{person}</h2>
             <li>
-              {indexes[person].gender},
-              {indexes[person].hair_color},
-              {indexes[person].height},
-              {indexes[person].mass}
+              {peopleInfo[person].gender},
+              {peopleInfo[person].hair_color},
+              {peopleInfo[person].height},
+              {peopleInfo[person].mass}
             </li>
           </div>
-          //add each height and mass for total mass and height
+          add each height and mass for total mass and height
         )
-      })}
+      })} */}
 
 
     </>
   );
-}
 
+}
 export default App;
--- src/components/people.js
+++ src/components/people.js
@@ -1,18 +1,33 @@
 import React from 'react'
-// import { Button, Card } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function People({ people, sortedNames }) {
+export default function People({ pageCompleted, page }) {
+    //print pageCompleted
+    let people = pageCompleted[page];
 
     return (
-        <>
-            {(() => {
-                for (let i = 0; i < sortedNames.length; i++) {
+        <div>
+            {
+                Object.keys(people).map((person, x) => {
+
                     return (
-                        <li>{people[sortedNames[i]].name}</li>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>{person}</Card.Title>
+                                <Card.Text>
+                                    <li>gender:     {people[person].gender}, </li>
+                                    <li>hair_color: {people[person].hair_color},</li>
+                                    <li>height:     {people[person].height},</li>
+                                    <li>mass:       {people[person].mass}</li>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+
                     )
-                }
-            })()}
-        </>
+
+                })
+            }
+        </div>
     )
 }
